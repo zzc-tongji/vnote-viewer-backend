1a4675871260b653a2edf0e3873a7efc1bb6a4c1
Zhicheng Zhang <zzc-tongji@users.noreply.github.com>
2020-06-06 00:40:07 -0400

    initialize

---
+++ README.md
@@ -0,0 +1,7 @@
+# vnote-viewer-backend
+
+### Others
+
+- All ".md" files are edited by Typora.
+- Style of all ".md" files is Github Flavored Markdown.
+- There is a LF (Linux) at end of each line.
---
+++ src/test/java/icu/zzc/vnote/DemoApplicationTests.java
@@ -0,0 +1,11 @@
+package icu.zzc.vnote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+  @Test
+  void contextLoads() {
+  }
+}
